87406abb06440725ed2da868c24777505bda39a3
AngelHCS <8990holbertonstudents.com>
2024-07-24 20:07:57 -0400

    8, 9 and 10

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string') {
+    throw new TypeError('startString must be a string');
+  }
+
+  return [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
