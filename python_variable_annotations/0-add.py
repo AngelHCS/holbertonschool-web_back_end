d5e596210a4ca34df432446ba452e72092dd6f0c
AngelHCS <8990holbertonstudents.com>
2024-08-14 20:08:59 -0400

    crude documentation on 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+"""user bin python 3"""
+
+
 def add(a: float, b: float) -> float:
-    # defines vars as floats
+    """Defines a function that makes sum of floats"""
     return a + b
