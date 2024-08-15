1b5a322ef5541f705200266ec7359c7357b5dc4d
AngelHCS <8990holbertonstudents.com>
2024-08-14 20:21:25 -0400

    changed syntax for checker

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = str("Holberton")
+
+"""Comment for checker"""
+
+
+a: int = (1)
+pi: float = (3.14)
+i_understand_annotations: bool = (True)
+school: str = ("Holberton")
