4b2c0b9a575f77d4d642c9f45e5adae97c68e8ee
AngelHCS <8990holbertonstudents.com>
2024-08-14 20:43:58 -0400

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""documentation for checker"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """calls function from typing library to use in function"""
+    return sum(input_list)
