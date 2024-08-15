860e0b3535345f8c21cea8d79928a90b137e5352
AngelHCS <8990holbertonstudents.com>
2024-08-14 20:13:36 -0400

    crude docmentation on 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+
+"""This is a comment for the checker"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """function takes 2 strings as args, result is str"""
     return str1+str2
