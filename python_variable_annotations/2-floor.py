f88d8f71959e5bf1299e991185b8eb46fba377de
AngelHCS <8990holbertonstudents.com>
2024-08-14 20:15:19 -0400

    crude documentation on 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import math
 
+"""Imports math library"""
+
 
 def floor(n: float) -> int:
+    """Function that has a float arg and int as a result"""
     return math.floor(n)
