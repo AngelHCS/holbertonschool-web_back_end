7614a744c02c21ae90b0af09da4f01f619db6031
AngelHCS <8990holbertonstudents.com>
2024-08-14 21:01:27 -0400

    6 sum list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import Union,List
+
+"""imports typing library"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ defines function that works with List"""
+    return sum(mxd_lst)
