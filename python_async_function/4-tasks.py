9569cde85219cb4a8ae2960f823e6d547d7fbb27
AngelHCS <8990holbertonstudents.com>
2024-08-15 23:42:49 -0400

    4 task

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+
+# intro comment (Libraries)
+
+
+async def wait_random(max_delay: int) -> float:
+
+    # func comment
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+
+    # func comment
+
+    return asyncio.create_task(wait_random(max_delay))
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+
+    # func comment
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
