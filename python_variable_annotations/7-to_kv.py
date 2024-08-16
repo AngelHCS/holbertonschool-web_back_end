1d72e95115565f77c1bf942202fd3e28c6fd403f
AngelHCS <8990holbertonstudents.com>
2024-08-15 21:38:09 -0400

    7 kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import Union, Tuple
+
+"""importing functions from typing"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Defines a function, K as a string,V as int or float returns tuple"""
+
+
+return (k, float(v ** 2))
