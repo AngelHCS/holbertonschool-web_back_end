f9622eeae43181dc87b8b56b9094ebb7e9db68dd
AngelHCS <8990holbertonstudents.com>
2024-08-23 23:17:45 -0400

    generator, numberbetween 0-10

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" setting python3 interpreter"""
+
+
+import random
+from typing import Generator
+import asyncio
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ generator function, num between 0-10 """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
